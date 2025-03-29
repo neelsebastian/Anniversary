6647b9cbf1aab3ff3a5be2475d6419ab57b58317
Neel Sebastian <68556380+neelsebastian@users.noreply.github.com>
2025-03-29 23:17:32 +0530

    script.js

--- script.js
+++ script.js
@@ -10,17 +10,5 @@ $(document).ready(function(){
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    let playButton = document.getElementById("play-button");
-    let audio = document.getElementById("bg-music");
-
-    playButton.addEventListener("click", function () {
-        audio.play();
-        playButton.textContent = "🎶 Playing..."; 
-        playButton.disabled = true; // 
-        playButton.style.opacity = "0.9"; 
-        playButton.style.cursor = "default"; 
-    });
-});
 
 
